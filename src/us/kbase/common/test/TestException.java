297440dcf8a0284beeb5c9b39b36bf132d2c2793
Gavin Price <gaprice@lbl.gov>
2013-09-30 15:20:07 -0700

    Add test exception class

---
+++ src/us/kbase/common/test/TestException.java
@@ -0,0 +1,16 @@
+package us.kbase.common.test;
+
+/** 
+ * Thrown when an exception occurs while setting up tests.
+ * @author gaprice@lbl.gov
+ *
+ */
+public class TestException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public TestException() { super(); }
+	public TestException(String message) { super(message); }
+	public TestException(String message, Throwable cause) { super(message, cause); }
+	public TestException(Throwable cause) { super(cause); }
+}
