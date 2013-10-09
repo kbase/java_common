cbc526e1352070f25025f51199b89f945b0afec1
Gavin Price <gaprice@lbl.gov>
2013-10-09 14:46:45 -0700

    Make timezone explicit when formatting dates

--- src/us/kbase/common/utils/ServiceUtils.java
+++ src/us/kbase/common/utils/ServiceUtils.java
@@ -1,15 +1,24 @@
 package us.kbase.common.utils;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * Miscellaneous code snippets common to java services.
+ * 
+ * @author gaprice@lbl.gov
+ *
+ */
 public class ServiceUtils {
 
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-
+	/**
+	 * Throws an exception if there are additional arguments in a java
+	 * typecompiler compiled class instance.
+	 * @param addlargs the getAdditionalProperties() output from a java 
+	 * typecompiler generated class.
+	 * @param clazz the class of the java typecompiler generated class.
+	 */
 	public static void checkAddlArgs(final Map<String, Object> addlargs,
 			@SuppressWarnings("rawtypes") final Class clazz) {
 		if (addlargs.isEmpty()) {
@@ -19,12 +28,5 @@ public class ServiceUtils {
 				"Unexpected arguments in %s: %s",
 				clazz.getName().substring(clazz.getName().lastIndexOf(".") + 1),
 				StringUtils.join(addlargs.keySet(), " ")));
-	}
-
-	public static String formatDate(final Date d) {
-		if (d == null) {
-			return null;
-		}
-		return DATE_FORMAT.format(d);
 	}
 }
---
+++ src/us/kbase/common/utils/UTCDateFormat.java
@@ -0,0 +1,54 @@
+package us.kbase.common.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * A formatter/parser for dates in the format "yyyy-MM-dd'T'HH:mm:ssZ".
+ * Date strings are always returned in UTC (+0000).
+ * 
+ * @author gaprice@lbl.gov
+ * 
+ * @see SimpleDateFormat
+ *
+ */
+public class UTCDateFormat {
+	
+	private final SimpleDateFormat format =
+			new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+
+	/**
+	 * The sole constructor.
+	 */
+	public UTCDateFormat() {
+		format.setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
+	
+	/**
+	 * Formats a date.
+	 * @param d the date.
+	 * @return the formatted date.
+	 */
+	public String formatDate(final Date d) {
+		if (d == null) {
+			return null;
+		}
+		return format.format(d);
+	}
+	
+	/**
+	 * Parses a date.
+	 * @param d the date string.
+	 * @return a date.
+	 * @throws ParseException if the date cannot be parsed.
+	 * @throws IllegalArgumentException if the date is null.
+	 */
+	public Date parseDate(final String d) throws ParseException {
+		if (d == null) {
+			throw new IllegalArgumentException("date string cannot be null");
+		}
+		return format.parse(d);
+	}
+}
