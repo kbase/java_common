d2d398c4466e38136bb4272a0dc2f73df98fc06a
Gavin Price <gaprice@lbl.gov>
2014-04-18 13:44:15 -0700

    make chart of results

--- src/us/kbase/common/performance/sortjson/MeasureSortRunner.java
+++ src/us/kbase/common/performance/sortjson/MeasureSortRunner.java
@@ -1,14 +1,38 @@
 package us.kbase.common.performance.sortjson;
 
+import java.awt.Color;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.LegendItem;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.block.Block;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.LegendTitle;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 
 
 public class MeasureSortRunner {
+	
+	final static boolean HEADLESS = true;
 	
 	final static int NUM_SORTS = 500;
 	final static int TIME_INTERVAL = 100; //ms
@@ -54,12 +78,73 @@ public class MeasureSortRunner {
 		int interval = TIME_INTERVAL;
 		String file = FILE;
 		
-		List<List<Double>> mems = new ArrayList<List<Double>>();
+		Map<String, List<Double>> mems = new HashMap<String, List<Double>>();
 		for (String sorter: SORTERS) {
 			System.out.println("Running sorter: " + sorter);
-			mems.add(runMeasureSort(numSorts, interval, file, sorter));
+			mems.put(sorter, runMeasureSort(numSorts, interval, file, sorter));
 		}
-		System.out.print(mems);
+		
+		String title = "Title";//TODO info
+		final JFreeChart chart = saveChart(new File("output.png"), mems, title);
+
+		
+
+		final FooFrame demo = new FooFrame("Title", chart);
+		demo.pack();
+		RefineryUtilities.centerFrameOnScreen(demo);
+		demo.setVisible(true);
+	}
+
+	private static JFreeChart saveChart(File f, Map<String, List<Double>> mems,
+			String title) throws IOException {
+		XYSeriesCollection xyc = new XYSeriesCollection();
+		for (String sorter: SORTERS) {
+			XYSeries s = new XYSeries(sorter, false, false);
+			double count = 1;
+			for (Double mem: mems.get(sorter)) {
+				s.add(++count, mem);
+			}
+			xyc.addSeries(s);
+		}
+		
+		final JFreeChart chart = ChartFactory.createXYLineChart(
+				title, 
+				"Measurement #",
+				"Used Memory (MB)",
+				xyc,
+				PlotOrientation.VERTICAL,
+				true, // include legend
+				true, // tooltips
+				false // urls
+				);
+		
+		chart.setBackgroundPaint(Color.white);
+
+		final LegendTitle legend = chart.getLegend();
+		for (Object li: legend.getItemContainer().getBlocks()) {
+			((LegendItem)li).setShapeVisible(true);
+		}
+
+		final XYPlot plot = chart.getXYPlot();
+		plot.setBackgroundPaint(Color.white);
+		plot.setDomainGridlinePaint(Color.gray);
+		plot.setRangeGridlinePaint(Color.gray);
+
+		final ValueAxis domainAxis = plot.getDomainAxis();
+		domainAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+		
+		ChartUtilities.saveChartAsPNG(f, chart, 700, 500);
+		return chart;
+	}
+	
+	private static class FooFrame extends ApplicationFrame {
+		
+		FooFrame(String title, JFreeChart chart) {
+			super(title);
+			final ChartPanel chartPanel = new ChartPanel(chart);
+//			chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+			setContentPane(chartPanel);
+		}
 	}
 
 	private static List<Double> runMeasureSort(int numSorts,
