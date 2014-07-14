5c57dba3b87f2f7e5054f2bf62244d8726ed948e
Gavin Price <gaprice@lbl.gov>
2014-07-14 13:01:49 -0700

    Add string checking fn

--- src/us/kbase/common/utils/StringUtils.java
+++ src/us/kbase/common/utils/StringUtils.java
@@ -61,4 +61,28 @@ public class StringUtils {
 			}
 		}
 	}
+	
+	/**
+	 * Checks that a string is below a maximum length and not empty. Null
+	 * strings are allowed.
+	 * @param s the string to check.
+	 * @param sname the name of the variable. This name will be used in the 
+	 * thrown exception.
+	 * @param length the maximum allowed length of the string.
+	 * @throws IllegalArgumentException if the string is longer than the
+	 * maximum length allowed or is empty.
+	 */
+	public static void checkMaxLenAndNonEmpty(final String s,
+			final String sname, final int length) {
+		if (s != null) {
+			if (s.isEmpty()) {
+				throw new IllegalArgumentException(sname + 
+						" cannot be the empty string");
+			}
+			if (s.length() > length) {
+				throw new IllegalArgumentException(sname + 
+						" exceeds the maximum length of " + length);
+			}
+		}
+	}
 }
