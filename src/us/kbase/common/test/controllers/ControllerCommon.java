e64880f5e85458f2962d293ef037a7345b7afa06
Gavin Price <gaprice@lbl.gov>
2014-08-13 12:19:32 -0700

    Add various controllers for tests

---
+++ src/us/kbase/common/test/controllers/ControllerCommon.java
@@ -0,0 +1,102 @@
+package us.kbase.common.test.controllers;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.List;
+import java.util.Set;
+
+import us.kbase.common.test.TestException;
+
+public class ControllerCommon {
+
+	/** See https://gist.github.com/vorburger/3429822
+	 * Returns a free port number on localhost.
+	 *
+	 * Heavily inspired from org.eclipse.jdt.launching.SocketUtil (to avoid a
+	 * dependency to JDT just because of this).
+	 * Slightly improved with close() missing in JDT. And throws exception
+	 * instead of returning -1.
+	 *
+	 * @return a free port number on localhost
+	 * @throws IllegalStateException if unable to find a free port
+	 */
+	public static int findFreePort() {
+		ServerSocket socket = null;
+		try {
+			socket = new ServerSocket(0);
+			socket.setReuseAddress(true);
+			int port = socket.getLocalPort();
+			try {
+				socket.close();
+			} catch (IOException e) {
+				// Ignore IOException on close()
+			}
+			return port;
+		} catch (IOException e) {
+		} finally {
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		throw new IllegalStateException(
+				"Could not find a free TCP/IP port");
+	}
+	
+	public static void checkExe(String exe, String exeType) {
+		File e = checkFile(exe, exeType, true);
+		if (!e.canExecute()) {
+			throw new IllegalArgumentException("The provided " + exeType +
+					" executable is not executable:" + exe);
+		}
+	}
+
+	public static File checkFile(String exe, String exeType) {
+		return checkFile(exe, exeType, false);
+	}
+	
+	public static File checkFile(String exe, String exeType,
+			boolean executable) {
+		
+		if (exe == null || exe.isEmpty()) {
+			throw new TestException(
+					(executable ? "Executable path for " : "Path for ") +
+							exeType + " cannot be null or the empty string ");
+		}
+		File e = new File(exe);
+		if (!e.exists()) {
+			throw new IllegalArgumentException("The provided " + exeType +
+					(executable ? " executable does not exist:" :
+						" path does not exist: ") + exe);
+		}
+		if (!e.isFile()) {
+			throw new IllegalArgumentException("The provided " + exeType +
+					(executable ? " executable is not a file:" :
+					" path is not a file: ") + exe);
+		}
+		return e;
+	}
+	
+	public static Path makeTempDirs(Path rootTempDir, String prefix,
+			List<String> subdirs)
+			throws IOException {
+		Files.createDirectories(rootTempDir.toAbsolutePath());
+		Set<PosixFilePermission> perms =
+				PosixFilePermissions.fromString("rwx------");
+		FileAttribute<Set<PosixFilePermission>> attr =
+				PosixFilePermissions.asFileAttribute(perms);
+		Path tempDir = Files.createTempDirectory(rootTempDir, prefix, attr);
+		for(String p: subdirs) {
+			Files.createDirectories(tempDir.resolve(p));
+		}
+		return tempDir;
+	}
+}
---
+++ src/us/kbase/common/test/controllers/shock/ShockController.java
@@ -0,0 +1,135 @@
+package us.kbase.common.test.controllers.shock;
+
+import static us.kbase.common.test.controllers.ControllerCommon.checkExe;
+import static us.kbase.common.test.controllers.ControllerCommon.findFreePort;
+import static us.kbase.common.test.controllers.ControllerCommon.makeTempDirs;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+
+/** Q&D Utility to run a Shock server for the purposes of testing from
+ * Java.
+ * @author gaprice@lbl.gov
+ *
+ */
+public class ShockController {
+	
+	//TODO share common code with AweController
+	
+	private final static String SHOCK_CONFIG_FN = "shock.cfg";
+	private final static String SHOCK_CONFIG =
+			"us/kbase/common/test/controllers/shock/conf/" +
+					SHOCK_CONFIG_FN;
+	
+	private final static List<String> tempDirectories =
+			new LinkedList<String>();
+	static {
+		tempDirectories.add("shock/site");
+		tempDirectories.add("shock/data");
+		tempDirectories.add("shock/logs");
+	}
+	
+	private final Path tempDir;
+	
+	private final Process shock;
+	private final int port;
+
+	public ShockController(
+			final String shockExe,
+			final Path rootTempDir,
+			final String adminUser,
+			final String mongohost,
+			final String shockMongoDBname,
+			final String mongouser,
+			final String mongopwd)
+					throws Exception {
+		tempDir = makeTempDirs(rootTempDir, "ShockController-", tempDirectories);
+		port = findFreePort();
+		
+		checkExe(shockExe, "shock server");
+		
+		Velocity.init();
+		VelocityContext context = new VelocityContext();
+		context.put("port", port);
+		context.put("tempdir", tempDir.toAbsolutePath().toString());
+		context.put("mongohost", mongohost);
+		context.put("mongodbname", shockMongoDBname);
+		context.put("mongouser", mongouser == null ? "" : mongouser);
+		context.put("mongopwd", mongopwd == null ? "" : mongopwd);
+		context.put("shockadmin", adminUser);
+		
+		File shockcfg = tempDir.resolve(SHOCK_CONFIG_FN).toFile();
+		
+		generateConfig(SHOCK_CONFIG, context, shockcfg);
+
+		ProcessBuilder servpb = new ProcessBuilder(shockExe, "--conf",
+				shockcfg.toString())
+				.redirectErrorStream(true)
+				.redirectOutput(tempDir.resolve("shock_server.log").toFile());
+		
+		shock = servpb.start();
+	}
+
+	public int getServerPort() {
+		return port;
+	}
+	
+	public Path getTempDir() {
+		return tempDir;
+	}
+	
+	public void destroy(boolean deleteTempFiles) throws IOException {
+		if (shock != null) {
+			shock.destroy();
+		}
+		if (tempDir != null && deleteTempFiles) {
+			FileUtils.deleteDirectory(tempDir.toFile());
+		}
+	}
+
+	private void generateConfig(final String configFile,
+			final VelocityContext context, File file)
+			throws IOException {
+		String template = IOUtils.toString(new BufferedReader(
+				new InputStreamReader(
+						getClass().getClassLoader()
+						.getResourceAsStream(configFile))));
+		
+		StringWriter sw = new StringWriter();
+		Velocity.evaluate(context, sw, "shockconfig", template);
+		PrintWriter pw = new PrintWriter(file);
+		pw.write(sw.toString());
+		pw.close();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		ShockController ac = new ShockController(
+				"/kb/deployment/bin/shock-server",
+				Paths.get("workspacetemp"),
+				"kbasetest2",
+				"localhost",
+				"delete_shock_db",
+				"foo", "foo");
+		System.out.println(ac.getServerPort());
+		Scanner reader = new Scanner(System.in);
+		System.out.println("any char to shut down");
+		//get user input for a
+		reader.next();
+		ac.destroy(false);
+	}
+	
+}
