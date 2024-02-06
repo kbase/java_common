82052d80d12eb6b3d6b144051bc4c1a835f7e7f6
Sijie <sijiex@lbl.gov>
2024-02-06 15:27:57 -0800

    update MongoController.java and .classpath

--- src/us/kbase/common/test/controllers/mongo/MongoController.java
+++ src/us/kbase/common/test/controllers/mongo/MongoController.java
@@ -4,76 +4,73 @@ import static us.kbase.common.test.controllers.ControllerCommon.checkExe;
 import static us.kbase.common.test.controllers.ControllerCommon.findFreePort;
 import static us.kbase.common.test.controllers.ControllerCommon.makeTempDirs;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
+import com.github.zafarkhaja.semver.Version;
 import org.apache.commons.io.FileUtils;
 
 
 /** Q&D Utility to run a Mongo server for the purposes of testing from
  * Java.
- * @author gaprice@lbl.gov
+ * @author gaprice@lbl.gov, sijiex@lbl.gov
  *
  */
 public class MongoController {
-	
+
 	private final static String DATA_DIR = "data";
-	
+
 	private final static List<String> tempDirectories =
 			new LinkedList<String>();
 	static {
 		tempDirectories.add(DATA_DIR);
 	}
-	
+
+	private final static Version MONGO_DB_6_1 =
+			Version.forIntegers(6,1);
+
 	private final Path tempDir;
-	
+
 	private final Process mongo;
+
 	private final int port;
 
 	public MongoController(
 			final String mongoExe,
 			final Path rootTempDir)
-					throws Exception {
+			throws Exception {
 		this(mongoExe, rootTempDir, false);
 	}
-	
+
 	public MongoController(
 			final String mongoExe,
 			final Path rootTempDir,
 			final boolean useWiredTiger)
-					throws Exception {
+			throws Exception {
 		checkExe(mongoExe, "mongod server");
 		tempDir = makeTempDirs(rootTempDir, "MongoController-", tempDirectories);
 		port = findFreePort();
-
-		List<String> command = new LinkedList<String>();
-		command.addAll(Arrays.asList(mongoExe, "--port", "" + port,
-				"--dbpath", tempDir.resolve(DATA_DIR).toString(),
-				"--nojournal"));
-		if (useWiredTiger) {
-			command.addAll(Arrays.asList("--storageEngine", "wiredTiger"));
-		}
-		ProcessBuilder servpb = new ProcessBuilder(command)
-				.redirectErrorStream(true)
-				.redirectOutput(tempDir.resolve("mongo.log").toFile());
-		
-		mongo = servpb.start();
-		Thread.sleep(1000); //wait for server to start up
+		Version dbVer = getMongoDBVer(mongoExe);
+		List<String> command = getMongoServerStartCommand(mongoExe, useWiredTiger, dbVer);
+		mongo = startProcess(command);
 	}
 
 	public int getServerPort() {
 		return port;
 	}
-	
+
 	public Path getTempDir() {
 		return tempDir;
 	}
-	
+
 	public void destroy(boolean deleteTempFiles) throws IOException, InterruptedException {
 		if (mongo != null) {
 			mongo.destroy();
@@ -90,6 +87,58 @@ public class MongoController {
 		}
 	}
 
+	private static Version getMongoDBVer(final String mongoExe) throws IOException {
+
+		// build MongoDB version check command
+		List<String> command = new LinkedList<String>();
+		command.addAll(Arrays.asList(mongoExe, "--version"));
+
+		// start MongoDB version check process
+		ProcessBuilder checkVerPb = new ProcessBuilder(command);
+		Process checkVerProcess  = checkVerPb.start();
+
+		// parse mongod --version output string
+		String dbVer = new BufferedReader(
+				new InputStreamReader(checkVerProcess.getInputStream()))
+				.lines()
+				.collect(Collectors.joining(" "))
+				.split(" ")[2].substring(1);
+
+		System.out.println("MongoDB version: " + dbVer);
+		checkVerProcess.destroy();
+		return Version.valueOf(dbVer);
+	}
+
+	private List<String> getMongoServerStartCommand(final String mongoExe,
+													final boolean useWiredTiger,
+													final Version dbVer) {
+		List<String> command = new LinkedList<String>();
+		command.addAll(Arrays.asList(mongoExe, "--port", "" + port,
+				"--dbpath", tempDir.resolve(DATA_DIR).toString()));
+
+		// Starting in MongoDB 6.1, journaling is always enabled.
+		// As a result, MongoDB removes the storage.journal.enabled option
+		// and the corresponding --journal and --nojournal command-line options.
+		// https://www.mongodb.com/docs/manual/release-notes/6.1/#changes-to-journaling
+		if (dbVer.lessThan(MONGO_DB_6_1)) {
+			command.addAll(Arrays.asList("--nojournal"));
+		}
+		if (useWiredTiger) {
+			command.addAll(Arrays.asList("--storageEngine", "wiredTiger"));
+		}
+		return command;
+	}
+
+	private Process startProcess(List<String> command) throws Exception {
+		ProcessBuilder servpb = new ProcessBuilder(command)
+				.redirectErrorStream(true)
+				.redirectOutput(getTempDir().resolve("mongo.log").toFile());
+
+		Process mongoProcess = servpb.start();
+		Thread.sleep(1000); //wait for server to start up
+		return mongoProcess;
+	}
+
 	public static void main(String[] args) throws Exception {
 		MongoController ac = new MongoController(
 				"/home/crushingismybusiness/mongo/3.6.12/bin/mongod",
@@ -103,5 +152,5 @@ public class MongoController {
 		ac.destroy(true);
 		reader.close();
 	}
-	
+
 }
