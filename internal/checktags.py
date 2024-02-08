d733c8b41337525c4e753fa53fd1ea294559a6b7
Sijie <sijiex@lbl.gov>
2024-02-07 21:29:09 -0800

    fix build errors

--- internal/checktags.py
+++ internal/checktags.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     for t in sys.argv[1:]:
         if VALID_TAG.match(t):
             if tag != "":
-                print "Two valid tags for this commit: {} {}".format(tag, t)
+                print("Two valid tags for this commit: {} {}".format(tag, t))
                 sys.exit(1)
             tag = t
-    print tag
+    print(tag)
